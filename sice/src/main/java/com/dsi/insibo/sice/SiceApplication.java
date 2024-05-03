43473a4034d841cfb0630dc8ebc48815334cfd1a
Manuel Ascencio <AA21050@ues.edu.sv>
2024-05-03 08:04:34 -0600

    agregando cambios

--- sice/src/main/java/com/dsi/insibo/sice/SiceApplication.java
+++ sice/src/main/java/com/dsi/insibo/sice/SiceApplication.java
@@ -2,12 +2,20 @@ package com.dsi.insibo.sice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class SiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiceApplication.class, args);
+	}
+
+	@GetMapping
+	public String holaMundo(){
+		return "prueba";
 	}
 
 }
