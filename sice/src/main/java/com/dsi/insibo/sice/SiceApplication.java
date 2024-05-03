1ad134bdc0e31022c5bae5b88d9a63d10cec3386
Manuel Ascencio <AA21050@ues.edu.sv>
2024-05-02 22:52:15 -0600

    Creación del proyecto

---
+++ sice/src/main/java/com/dsi/insibo/sice/SiceApplication.java
@@ -0,0 +1,13 @@
+package com.dsi.insibo.sice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiceApplication.class, args);
+	}
+
+}
