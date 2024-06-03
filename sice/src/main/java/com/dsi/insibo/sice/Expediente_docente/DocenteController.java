86014a3934467f611a4a80fa07aa43f83805b122
PM21005 <116988782+darluk1@users.noreply.github.com>
2024-06-02 20:37:32 -0600

    Vista ficha docente

---
+++ sice/src/main/java/com/dsi/insibo/sice/Expediente_docente/DocenteController.java
@@ -0,0 +1,18 @@
+package com.dsi.insibo.sice.Expediente_docente;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class DocenteController {
+
+
+    @GetMapping("/zzz")
+    public String docentes() {
+        return "Expediente_docente/fichaDocente";
+    }
+
+
+}
