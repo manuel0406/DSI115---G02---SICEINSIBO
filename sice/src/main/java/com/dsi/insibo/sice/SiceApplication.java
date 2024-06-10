6ee5f3ac4c295b99432fceba88f66fcbb046b717
Manuel Ascencio <AA21050@ues.edu.sv>
2024-06-10 14:04:32 -0600

    Update SiceApplication.java

--- sice/src/main/java/com/dsi/insibo/sice/SiceApplication.java
+++ sice/src/main/java/com/dsi/insibo/sice/SiceApplication.java
@@ -3,6 +3,7 @@ package com.dsi.insibo.sice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -16,7 +17,8 @@ public class SiceApplication {
 	}
 
 	@GetMapping("/home")
-	public String holamundo() {
+	public String holamundo( Model model) {
+		model.addAttribute("titulo", "Inicio");
 		return "home";
 	}
 	
