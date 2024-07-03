2ccd57bbd128464fc2769f829cf667fc981cbf1e
PM21005 <116988782+darluk1@users.noreply.github.com>
2024-07-02 20:46:43 -0600

    Manejo de anexos para el personal docente

--- sice/src/main/java/com/dsi/insibo/sice/Expediente_docente/Docentes/Anexos/AnexoDocenteController.java
+++ sice/src/main/java/com/dsi/insibo/sice/Expediente_docente/Docentes/Anexos/AnexoDocenteController.java
@@ -1,4 +1,4 @@
-package com.dsi.insibo.sice.Expediente_docente.Administrativos.Anexos;
+package com.dsi.insibo.sice.Expediente_docente.Docentes.Anexos;
 
 import java.io.IOException;
 import java.util.Date;
@@ -17,24 +17,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.dsi.insibo.sice.Expediente_docente.Administrativos.AdministrativoService;
-import com.dsi.insibo.sice.entity.AnexoPersonalAdministrativo;
-import com.dsi.insibo.sice.entity.PersonalAdministrativo;
+import com.dsi.insibo.sice.Expediente_docente.Docentes.DocenteService;
+import com.dsi.insibo.sice.entity.AnexoDocente;
+import com.dsi.insibo.sice.entity.Docente;
 
 @Controller
-public class AnexoAdministrativoController {
+public class AnexoDocenteController {
 
     @Autowired
-    private AnexoAdministrativoService anexoService;
+    private AnexoDocenteService anexoService;
     @Autowired
-    private AnexoAdministrativoRepository anexoRepository;
+    private AnexoDocenteRepository anexoRepository;
     @Autowired
-    private AdministrativoService administrativoService;
+    private DocenteService docenteService;
 
     // Controlador para subir archivos
-    @GetMapping("/administrativo/Subir/{clave}/{duiPersonal}")
+    @GetMapping("/docente/Subir/{clave}/{duiDocente}")
     public String subirAdministrativo(@PathVariable("clave") String clave,
-            @PathVariable("duiPersonal") String duiPersonal, Model model) {
+            @PathVariable("duiDocente") String duiDocente, Model model) {
         if (clave.equals("dui")) {
             model.addAttribute("tituloB", "Seleccione su DUI");
         } else if (clave.equals("curriculum")) {
@@ -43,51 +43,51 @@ public class AnexoAdministrativoController {
             model.addAttribute("tituloB", "Seleccione su NUP");
         }
 
-        // Agrega los atributos "clave" y "duiPersonal" al modelo
+        // Agrega los atributos "clave" y "duiDocente" al modelo
         model.addAttribute("clave", clave);
-        model.addAttribute("duiPersonal", duiPersonal);
+        model.addAttribute("duiDocente", duiDocente);
         model.addAttribute("titulo", "Documentos");
-        model.addAttribute("enlace", "/expedienteadministrativo/Documentos/" + duiPersonal);
-        System.out.println("duiPersonal: " + duiPersonal); // Imprime el duiPersonal en la consola
-        return "Expediente_docente/Administrativos/upload";
+        model.addAttribute("enlace", "/expedientedocente/Documentos/" + duiDocente); // cambiar por el del controller
+        System.out.println("duiDocente: " + duiDocente); // Imprime el duiDocente en la consola
+        return "Expediente_docente/Docentes/upload";
         //return "upload";
 
     }
 
     // Controlador para mostrar el estado de la carga de archivos.
-    @GetMapping("/administrativo/uploadStatus")
+    @GetMapping("/docente/uploadStatus")
     public String uploadStatus(Model model) {
-        return "Expediente_docente/Administrativos/uploadStatushtml";
+        return "Expediente_docente/Docentes/uploadStatushtml";
     }
 
     // Controlador para cargar archivos
-    @PostMapping("/administrativo/upload/{clave}/{duiPersonal}")
-    public String cargandoArchivo(@PathVariable("clave") String clave, @PathVariable("duiPersonal") String duiPersonal,
+    @PostMapping("/docente/upload/{clave}/{duiDocente}")
+    public String cargandoArchivo(@PathVariable("clave") String clave, @PathVariable("duiDocente") String duiDocente,
             @RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 
-        // Busca al administrativo por su duiPersonal y obtiene sus anexos
-        PersonalAdministrativo administrativo = administrativoService.buscarPorIdAdministrativo(duiPersonal);
-        AnexoPersonalAdministrativo archivo = anexoService.buscarAdministrativo(duiPersonal);
+        // Busca al administrativo por su duiDocente y obtiene sus anexos
+        Docente docente = docenteService.buscarPorIdDocente(duiDocente);
+        AnexoDocente archivo = anexoService.buscarDocente(duiDocente);
 
         // Si el anexo es nulo, crea uno nuevo
         if (archivo == null) {
-            archivo = new AnexoPersonalAdministrativo();
+            archivo = new AnexoDocente();
         }
 
         // Cuando la clave es DUI
         if (clave.equals("dui")) {
             if (file.isEmpty()) {
                 redirectAttributes.addFlashAttribute("success", "Por favor, seleccione un archivo para subir.");
-                return "redirect:/expedienteadministrativo/plantaadministrativa";
+                return "redirect:/expedientedocente/plantadocente";
             }
             try {
                 byte[] bytes = file.getBytes(); // Lee los bytes del archivo
 
                 // Guarda la referencia en la base de datos
-                archivo.setNombreDuiPersonal(file.getOriginalFilename());
-                archivo.setDatosDuiPersonal(bytes);
-                archivo.setFechaDuiPersonal(new Date());
-                archivo.setPersonalAdministrativo(administrativo);
+                archivo.setNombreDuiDocente(file.getOriginalFilename());
+                archivo.setDatosDuiDocente(bytes);
+                archivo.setFechaDuiDocente(new Date());
+                archivo.setDocente(docente);
                 anexoService.guardaAnexo(archivo);
 
                 redirectAttributes.addFlashAttribute("success",
@@ -95,23 +95,23 @@ public class AnexoAdministrativoController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return "redirect:/expedienteadministrativo/Documentos/" + duiPersonal;
+            return "redirect:/expedientedocente/Documentos/" + duiDocente; // cambiar por el del controller
         }
 
         // Cuando la clave es Curriculum
         else if (clave.equals("curriculum")) {
             if (file.isEmpty()) {
                 redirectAttributes.addFlashAttribute("success", "Por favor, seleccione un archivo para subir.");
-                return "redirect:/expedienteadministrativo/plantaadministrativa";
+                return "redirect:/expedientedocente/plantadocente";
             }
             try {
                 byte[] bytes = file.getBytes();
 
                 // Guarda la referencia en la base de datos
-                archivo.setNombreCurriculumPersonal(file.getOriginalFilename());
-                archivo.setDatosCurriculumPersonal(bytes);
-                archivo.setFechaCurriculumPersonal(new Date());
-                archivo.setPersonalAdministrativo(administrativo);
+                archivo.setNombreCurriculumDocente(file.getOriginalFilename());
+                archivo.setDatosCurriculumDocente(bytes);
+                archivo.setFechaCurriculumDocente(new Date());
+                archivo.setDocente(docente);
                 anexoService.guardaAnexo(archivo);
 
                 redirectAttributes.addFlashAttribute("success",
@@ -119,23 +119,23 @@ public class AnexoAdministrativoController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return "redirect:/expedienteadministrativo/Documentos/" + duiPersonal;
+            return "redirect:/expedientedocente/Documentos/" + duiDocente; // cambiar por el del controller
         }
 
         // Cuando la clave es NUP
         else if (clave.equals("nup")) {
             if (file.isEmpty()) {
                 redirectAttributes.addFlashAttribute("success", "Por favor, seleccione un archivo para subir.");
-                return "redirect:/expedienteadministrativo/plantaadministrativa";
+                return "redirect:/expedientedocente/plantadocente";
             }
             try {
                 byte[] bytes = file.getBytes();
 
                 // Guarda la referencia en la base de datos
-                archivo.setNombreNupPersonal(file.getOriginalFilename());
-                archivo.setDatosNupPersonal(bytes);
-                archivo.setFechaNupPersonal(new Date());
-                archivo.setPersonalAdministrativo(administrativo);
+                archivo.setNombreNupDocente(file.getOriginalFilename());
+                archivo.setDatosNupDocente(bytes);
+                archivo.setFechaNupDocente(new Date());
+                archivo.setDocente(docente);
                 anexoService.guardaAnexo(archivo);
 
                 redirectAttributes.addFlashAttribute("success",
@@ -143,74 +143,74 @@ public class AnexoAdministrativoController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return "redirect:/expedienteadministrativo/Documentos/" + duiPersonal;
+            return "redirect:/expedientedocente/Documentos/" + duiDocente; // cambiar por el del controller
         }
-        return "redirect:/expedienteadministrativo/plantaadministrativa";
+        return "redirect:/expedientedocente/plantadocente";
     }
 
     // Controlador para eliminar archivos
-    @GetMapping("/administrativo/delete/{clave}/{id}/{duiPersonal}")
+    @GetMapping("/docente/delete/{clave}/{id}/{duiDocente}")
     public String deleteAnexo(@PathVariable("clave") String clave, @PathVariable("id") int id, Model model,
-            @PathVariable("duiPersonal") String duiPersonal, RedirectAttributes redirectAttributes) {
+            @PathVariable("duiDocente") String duiDocente, RedirectAttributes redirectAttributes) {
 
         // Obtiene los anexos del administrativo
-        AnexoPersonalAdministrativo archivo = anexoService.buscarAnexoPorId(id);
+        AnexoDocente archivo = anexoService.buscarAnexoPorId(id);
 
         // Cuando la clave es DUI
         if (clave.equals("dui")) {
             // Elimina los datos relacionados con el DUI
-            archivo.setNombreDuiPersonal(null);
-            archivo.setDatosDuiPersonal(null);
-            archivo.setFechaDuiPersonal(null);
+            archivo.setNombreDuiDocente(null);
+            archivo.setDatosDuiDocente(null);
+            archivo.setFechaDuiDocente(null);
             anexoService.guardaAnexo(archivo);
             redirectAttributes.addFlashAttribute("warning", "Se ha eliminado correctamente el archivo ");
 
-            return "redirect:/expedienteadministrativo/Documentos/" + duiPersonal;
+            return "redirect:/expedientedocente/Documentos/" + duiDocente; // cambiar por el del controller
         }
 
         // Cuando la clave es curriculum
         else if (clave.equals("curriculum")) {
             // Elimina los datos relacionados con el curriculum
-            archivo.setNombreCurriculumPersonal(null);
-            archivo.setDatosCurriculumPersonal(null);
-            archivo.setFechaCurriculumPersonal(null);
+            archivo.setNombreCurriculumDocente(null);
+            archivo.setDatosCurriculumDocente(null);
+            archivo.setFechaCurriculumDocente(null);
             anexoService.guardaAnexo(archivo);
             redirectAttributes.addFlashAttribute("warning", "Se ha eliminado correctamente el archivo ");
 
-            return "redirect:/expedienteadministrativo/Documentos/" + duiPersonal;
+            return "redirect:/expedientedocente/Documentos/" + duiDocente;
         }
 
         // Cuando la clave es NUP
         else if (clave.equals("nup")) {
             // Elimina los datos relacionados con el curriculum
-            archivo.setNombreNupPersonal(null);
-            archivo.setDatosNupPersonal(null);
-            archivo.setFechaNupPersonal(null);
+            archivo.setNombreNupDocente(null);
+            archivo.setDatosNupDocente(null);
+            archivo.setFechaNupDocente(null);
             anexoService.guardaAnexo(archivo);
             redirectAttributes.addFlashAttribute("warning", "Se ha eliminado correctamente el archivo ");
 
-            return "redirect:/expedienteadministrativo/Documentos/" + duiPersonal;
+            return "redirect:/expedientedocente/Documentos/" + duiDocente;
         }
         
-        return "Expediente_administrativo/upload";
+        return "Expediente_docente/upload";
     }
 
     // Controlador para mostrar archivos
-    @GetMapping("/administrativo/expedienteadministrativo/files/{clave}/{id}")
+    @GetMapping("/docente/expedientedocente/files/{clave}/{id}")
     @ResponseBody
     public ResponseEntity<ByteArrayResource> mostrarAnexo(@PathVariable int id, @PathVariable("clave") String clave) {
         // Busca el archivo en el repositorio por su ID
-        AnexoPersonalAdministrativo fileEntity = anexoRepository.findById(id)
+        AnexoDocente fileEntity = anexoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("File not found"));
 
         // Cuando la clave es DUI
         if (clave.equals("dui")) {
             // Crea un recurso de bytes a partir de los datos del archivo
-            ByteArrayResource resource = new ByteArrayResource(fileEntity.getDatosDuiPersonal());
+            ByteArrayResource resource = new ByteArrayResource(fileEntity.getDatosDuiDocente());
             // Devuelve la respuesta con el contenido del archivo
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION,
-                            "inline; filename=\"" + fileEntity.getNombreDuiPersonal() + "\"")
+                            "inline; filename=\"" + fileEntity.getNombreDuiDocente() + "\"")
                     .contentType(MediaType.APPLICATION_PDF)
                     .body(resource);
         }
@@ -218,11 +218,11 @@ public class AnexoAdministrativoController {
         // Cuando la clave es curriculum
         else if (clave.equals("curriculum")) {
             // Crea un recurso de bytes a partir de los datos del archivo
-            ByteArrayResource resource = new ByteArrayResource(fileEntity.getDatosCurriculumPersonal());
+            ByteArrayResource resource = new ByteArrayResource(fileEntity.getDatosCurriculumDocente());
             // Devuelve la respuesta con el contenido del archivo
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION,
-                            "inline; filename=\"" + fileEntity.getNombreCurriculumPersonal() + "\"")
+                            "inline; filename=\"" + fileEntity.getNombreCurriculumDocente() + "\"")
                     .contentType(MediaType.APPLICATION_PDF)
                     .body(resource);
         }
@@ -230,11 +230,11 @@ public class AnexoAdministrativoController {
         // Cuando la clave es nup
         else if (clave.equals("nup")) {
             // Crea un recurso de bytes a partir de los datos del archivo
-            ByteArrayResource resource = new ByteArrayResource(fileEntity.getDatosNupPersonal());
+            ByteArrayResource resource = new ByteArrayResource(fileEntity.getDatosNupDocente());
             // Devuelve la respuesta con el contenido del archivo
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION,
-                            "inline; filename=\"" + fileEntity.getDatosNupPersonal() + "\"")
+                            "inline; filename=\"" + fileEntity.getDatosNupDocente() + "\"")
                     .contentType(MediaType.APPLICATION_PDF)
                     .body(resource);
         }
