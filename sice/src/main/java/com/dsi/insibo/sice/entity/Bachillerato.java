ca58c6413ee53bf3d39f594242757f880b280a73
Manuel Ascencio <AA21050@ues.edu.sv>
2024-07-17 11:02:11 -0600

    Agregando nuevas entidades
    Se creo la entidad Asignacion, AnioAcademico.
    También se agregaron las nuevas relaciones y se elimino la relación que existia de docente en materia

--- sice/src/main/java/com/dsi/insibo/sice/entity/Bachillerato.java
+++ sice/src/main/java/com/dsi/insibo/sice/entity/Bachillerato.java
@@ -4,43 +4,75 @@ import java.io.Serializable;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Bachillerato implements Serializable{
+public class Bachillerato implements Serializable {
+
     @Id
     private String codigoBachillerato;
     private String nombreCarrera;
     private String seccion;
     private int grado;
+    @ManyToOne
+    private AnioAcademico anioAcademico;
+
+    public Bachillerato() {
+    }
+
+    public Bachillerato(String codigoBachillerato, String nombreCarrera, String seccion, int grado,
+            AnioAcademico anioAcademico) {
+        this.codigoBachillerato = codigoBachillerato;
+        this.nombreCarrera = nombreCarrera;
+        this.seccion = seccion;
+        this.grado = grado;
+        this.anioAcademico = anioAcademico;
+    }
 
     public String getCodigoBachillerato() {
         return codigoBachillerato;
     }
+
     public void setCodigoBachillerato(String codigoBachillerato) {
         this.codigoBachillerato = codigoBachillerato;
     }
+
     public String getNombreCarrera() {
         return nombreCarrera;
     }
+
     public void setNombreCarrera(String nombreCarrera) {
         this.nombreCarrera = nombreCarrera;
     }
+
     public String getSeccion() {
         return seccion;
     }
+
     public void setSeccion(String seccion) {
         this.seccion = seccion;
     }
+
     public int getGrado() {
         return grado;
     }
+
     public void setGrado(int grado) {
         this.grado = grado;
     }
+
+    public AnioAcademico getAnioAcademico() {
+        return anioAcademico;
+    }
+
+    public void setAnioAcademico(AnioAcademico anioAcademico) {
+        this.anioAcademico = anioAcademico;
+    }
+
     @Override
     public String toString() {
         return "Bachillerato [codigoBachillerato=" + codigoBachillerato + ", nombreCarrera=" + nombreCarrera
                 + ", seccion=" + seccion + ", grado=" + grado + "]";
     }
-    
+
 }
--- sice/src/main/java/com/dsi/insibo/sice/entity/Materia.java
+++ sice/src/main/java/com/dsi/insibo/sice/entity/Materia.java
@@ -1,46 +1,61 @@
 package com.dsi.insibo.sice.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Materia {
-    
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMateria;
     private String codMateria;
-
-    @ManyToOne
-   
-    private Docente docente;
-
     private String nomMateria;
     private String tipoMateria;
-    public String getCodMateria() {
-        return codMateria;
+
+    public Materia() {
     }
-    public void setCodMateria(String codMateria) {
+
+    public Materia(int idMateria, String codMateria, String nomMateria, String tipoMateria) {
+        this.idMateria = idMateria;
         this.codMateria = codMateria;
-    }
-    public Docente getDocente() {
-        return docente;
-    }
-    public void setDocente(Docente docente) {
-        this.docente = docente;
-    }
-    public String getNomMateria() {
-        return nomMateria;
-    }
-    public void setNomMateria(String nomMateria) {
         this.nomMateria = nomMateria;
-    }
-    public String getTipoMateria() {
-        return tipoMateria;
-    }
-    public void setTipoMateria(String tipoMateria) {
         this.tipoMateria = tipoMateria;
     }
 
-    
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getCodMateria() {
+        return codMateria;
+    }
+
+    public void setCodMateria(String codMateria) {
+        this.codMateria = codMateria;
+    }
+
+    public String getNomMateria() {
+        return nomMateria;
+    }
+
+    public void setNomMateria(String nomMateria) {
+        this.nomMateria = nomMateria;
+    }
+
+    public String getTipoMateria() {
+        return tipoMateria;
+    }
+
+    public void setTipoMateria(String tipoMateria) {
+        this.tipoMateria = tipoMateria;
+    }
 
 }
